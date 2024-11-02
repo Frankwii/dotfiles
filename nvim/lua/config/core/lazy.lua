e95a38bdb4c8364cce9609fb4f321119c17bd1c2
Frankwii <frank.william.hammond@gmail.com>
2024-11-02 14:42:39 +0100

    Been a while since last commit

--- nvim/lua/config/core/lazy.lua
+++ nvim/lua/config/core/lazy.lua
@@ -31,5 +31,6 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   -- install = { colorscheme = { "desert" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true,
+              notify = false},
 })
